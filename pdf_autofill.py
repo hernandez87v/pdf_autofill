6086e8c513ef55b737985a63945edef783c24172
hernandez87v <hernandez87v@gmail.com>
2022-02-25 13:31:14 -0800

    fix script which is now working for t2200.pdf

--- pdf_autofill.py
+++ pdf_autofill.py
@@ -3,7 +3,6 @@ import os
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from PyPDF2.generic import BooleanObject, NameObject, IndirectObject
 
-
 def set_need_appearances_writer(writer: PdfFileWriter):
     try:
         catalog = writer._root_object
@@ -17,16 +16,16 @@ def set_need_appearances_writer(writer: PdfFileWriter):
     except Exception as e:
         print('set_need_appearances_writer() catch : ', repr(e))
         return writer
-
+    
 if __name__ == '__main__':
     csv_filename = "autofill_data.csv"
-    pdf_filename = "t2200-fill-21e.pdf"
-    
+    pdf_filename = "output.pdf"
+
     csvin = os.path.normpath(os.path.join(os.getcwd(),'in',csv_filename))
     pdfin = os.path.normpath(os.path.join(os.getcwd(),'in',pdf_filename))
     pdfout = os.path.normpath(os.path.join(os.getcwd(),'out'))
     data = pd.read_csv(csvin)
-    pdf = PdfFileReader(open(pdfin, "rb"), strict=False)  
+    pdf = PdfFileReader(open(pdfin, "rb"))  
     if "/AcroForm" in pdf.trailer["/Root"]:
         pdf.trailer["/Root"]["/AcroForm"].update(
             {NameObject("/NeedAppearances"): BooleanObject(True)})
@@ -41,6 +40,8 @@ if __name__ == '__main__':
         if "/AcroForm" in pdf2._root_object:
             pdf2._root_object["/AcroForm"].update(
                 {NameObject("/NeedAppearances"): BooleanObject(True)})
+        
+        # Key = pdf_field_name : Value = csv_field_value
         # Below you must define the field names as keys in this dictionary
         # Field names found by running and printing line 15
         # Key = pdf_field_name : Value = csv_field_value
@@ -48,7 +49,6 @@ if __name__ == '__main__':
                             "First name": rows['FirstName'],
                             "Tax year": rows['TaxYear'],
                             "Employer address": rows['EmployerAddress'],
-                            "Post Code": rows['PostCode'],
                             "Job title and brief description of duties": rows['JobTitleAndBriefDescriptionOfDuties'],
                             }
 
@@ -57,7 +57,6 @@ if __name__ == '__main__':
         pdf2.updatePageFormFieldValues(pdf2.getPage(0), field_dictionary_1)
         pdf2.addPage(pdf.getPage(1))
         pdf2.addPage(pdf.getPage(2))
-        pdf2.addPage(pdf.getPage(3))
         outputStream = open(temp_out_dir, "wb")
         pdf2.write(outputStream)
         outputStream.close()
